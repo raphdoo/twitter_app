47fce21d7aa79901fba3515b1d89488165e83b47
Rapheal Ajiboye <raphoooo95@gmail.com>
2022-11-16 14:39:34 +0100

    setting the pug template engine view

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 
 const port = 3003
 
+app.set("view engine", "pug")
+app.set("views", "views")
+
 const server = app.listen(port, ()=>{
     console.log(`Server is listening at port ${port} `)
+})
+
+app.get('/', (req,res,next)=>{
+    res.status(200).send('Hello')
 })
