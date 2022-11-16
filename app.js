1b85472be28637820c168b2d3873c5d87016938f
Rapheal Ajiboye <raphoooo95@gmail.com>
2022-11-16 15:22:42 +0100

    setting up the rendering home page

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
 
+const { requireLogin } = require('./middleware/authenticate') 
+
 const port = 3003
 
+
+//setting up the view engine
 app.set("view engine", "pug")
 app.set("views", "views")
+
 
 const server = app.listen(port, ()=>{
     console.log(`Server is listening at port ${port} `)
 })
 
 app.get('/', (req,res,next)=>{
-    res.status(200).send('Hello')
+
+    const payload = {
+        title: "home"
+    }
+
+    res.status(200).render('home', payload)
 })
